728013eeb7581898ad1738f962675c05ea7f5e8e
anika ahsan <anika.ahsan@g.bracu.ac.bd>
2023-02-07 20:01:37 -0500

    product app created

--- django_project_1/settings.py
+++ django_project_1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'product'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'django_project_1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_project_1',
+        'HOST':'localhost',
+        'USER':'root',
+        'PASSWORD':'anikaahsan'
+
     }
 }
 
